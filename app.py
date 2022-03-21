95323de89f2ed1675cd4f40ea6a09d8f3174d9be
iamgillespie <gailen.gillespie@gmail.com>
2022-03-20 21:21:08 -0500

    reverting previous change

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
-
 @app.route("/result", methods=["POST"])
 def result():
 
